f1720f8a70104eae2c1e38a8b075fca30f01fc95
thincen <no_1seed@163.com>
2021-02-09 23:48:30 +0800

    更换为企业微信

--- Euthyrox.sh
+++ Euthyrox.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-CPSKEY=""
+# 接口access_token
+ACCESSTOKEN=""
+# 应用ID
+AGENTID=0
+# 公司ID
+CORID=""
+# 应用Cecret
+APPSECRET=""
+
 DEBUG=1
 
 D=$(($(TZ='Asia/Shanghai' date -d "12:00:00" +%s)/86400))
@@ -19,8 +27,28 @@ fi
 
 DATE=$(TZ='Asia/Shanghai' date +%F' '%T)
 
-RES=$(curl -sX POST -d"${DATE}"$'\n\n'"今日剂量：${DOSAGE}" https://push.xuthus.cc/send/${CPSKEY})
-
-if [ $(echo ${RES}|grep -E \"code\":200|wc -l) -lt 1 ];then
-	echo ${RES}
+if [ "${ACCESSTOKEN}" == "" ];then
+	RESTOKEN=$(curl -s "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=${CORID}&corpsecret=${APPSECRET}")
+	if [ $(echo ${RESTOKEN}|grep -E \"errcode\":0|wc -l) -lt 1 ];then
+		echo "Get Access_Token error:\n${RESTOEKN}"
+		exit 521
+	fi
+	ACCESSTOKEN=$(echo ${RESTOKEN#*\"access_token\"\:}|cut -d'"' -f2)
 fi
+
+# echo ${ACCESSTOKEN}
+
+CONTENT="Euthyrox\n\n$(TZ='Asia/Shanghai' date +%F)\n$(TZ='Asia/Shanghai' date +%T)运行完成\n\n剂量：${DOSAGE}"
+
+POSTRAW='{"touser":"@all","msgtype":"text","agentid":"'${AGENTID}'","text":{"content":"'${CONTENT}'"}}'
+# echo ${POSTRAW}
+
+RESPUSH=$(curl -sX POST "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=${ACCESSTOKEN}" \
+-H"Content-type: application/json" \
+-d"${POSTRAW}")
+
+if [ $(echo ${RESPUSH}|grep -E \"code\":0|wc -l) -lt 1 ];then
+	echo ${RES}
+	exit 522
+fi
+echo "push success"
